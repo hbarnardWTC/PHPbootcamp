05fb35b3af4ffdccabbf5b6e235df7ede5e88808
hbarnardWTC <wingarkorin@gmail.com>
2019-10-11 16:46:10 +0200

    day03

---
+++ day03/ex06/members_only.php
@@ -0,0 +1,11 @@
+<?PHP
+   if (($_SERVER['PHP_AUTH_USER'] == "zaz" && $_SERVER['PHP_AUTH_PW'] == "Ilovemylittleponey")) {
+        $file = file_get_contents('../img/42.png');
+        echo "<html><body>\nHello Zaz<br />\n<img src='data:image/jpeg;base64,".base64_encode($file)."'>\n</body></html>", PHP_EOL;
+   }
+   else {
+        header('HTTP/1.0 401 Unauthorized');
+        header('WWW-Authenticate: Basic realm=\'\'Member area\'\'');
+        echo "<html><body>That area is accessible for members only</body></html>", PHP_EOL;
+   }
+?>
