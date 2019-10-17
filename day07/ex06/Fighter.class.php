204c04f25f378f13affb92c95cef22e54616607d
hbarnardWTC <wingarkorin@gmail.com>
2019-10-17 18:50:46 +0200

    Finished

---
+++ day07/ex06/Fighter.class.php
@@ -0,0 +1,17 @@
+<?php
+	class Fighter {
+		private $name;
+		public function __construct($str) {
+			if ($str) {
+				$this->$name = $str;
+			} else {
+				$this->$name = NULL;
+			}
+		}
+		public function __tostring() {
+			if ($this->$name) {
+				return($this->$name);
+			}
+		}
+	}
+?>
